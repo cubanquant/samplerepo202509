639b31089ee201c288cc9328a342450934004b6f
cuban.quant <cuban.quant@gmail.com>
2025-09-03 21:01:29 -0400

    first commit

---
+++ assignment1_part2.py
@@ -0,0 +1,12 @@
+class Book:
+    def __init__(self, author, title):
+        pass
+
+    def display(self):
+        pass
+
+
+if __name__ == "__main__":
+    a = Book("Thorpe", "Beat the Dealer")
+    print("This is assignment 1 part 2")
+    print("this is a sample repository")
